f50c61cce668491277da712993ea2367359d72ca
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-09 16:23:32 +0200

    exercice 2 classer eleves

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -1,14 +1,84 @@
 require_relative "wagon_sort"
 
-students = []
+#le terminal me demande d'entrer un nom
+#je rentre une réponse
+#si cette réponse est vide
+#j'affiche les infos
+#sinon je reboucle
 
-begin
-  puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
-  name = gets
-  name.chomp! if name
+students_list = []
+puts "Entre un premier nom ? ou tape <entrer>"
+name = gets.chomp
 
-  # TODO: Add the name we just gotto the students array
+until name == ""
+  students_list << name
+  puts "Entre un nom ou quitter avec <entrer>"
+  name = gets.chomp
+end
 
-end while name != ""
+#########Afficher le message congrats
+if students_list.size == 1
+  congrats_message = "Congratulations ! your wagon has 1 student"
+else
+  congrats_message = "Congratulations! your wagon has#{students_list.size} students"
+end
 
-# TODO: Call `wagon_sort` method and display the sorted student list
+puts congrats_message
+
+#trier le tableau
+sorted_students = wagon_sort(students_list)
+
+#construire le message 'alice, bob, jule and pepita'
+message = ""
+
+for student in sorted_students
+    #on va ajouter l'étudiant à notre message avec la virgule
+
+    #si c est le dernier
+    if student == sorted_students.last
+      message += student
+
+    #si c est l'avant dernier
+  elsif student == sorted_students[sorted_students.size - 2]
+    message += student + " and "
+
+    #sinon
+    else
+      message += student + ", "
+    end
+end
+
+    puts message
+
+
+
+    #avec join
+
+    first_part = sorted_students[0..(sorted_students.size - 2)].join(", ")
+
+    final_message = "#{first_part} and #{sorted_students.last}"
+
+    puts final_message
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,4 @@
 def wagon_sort(students)
   # TODO: return a copy of students, sorted alphabetically
+  students.sort_by { |student| student.upcase }
 end
