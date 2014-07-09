c647a037cbc72d705dd5a26e3e0e160f774bddb2
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-09 18:30:18 +0200

    execrice balckjack

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,9 +1,11 @@
 def bank_score
   # TODO: Use Random to get a new random score
+  rand(16..21)
 end
 
 def pick_card
   # TODO: Use Random to get a new random card
+  rand(1..11)
 end
 
 def game_outcome(bank, score)
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,14 +2,31 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+bank = bank_score
+player_score = 0
+
+  while asking_for_card?(player_score)
+  player_score += pick_card
+  state_of_the_game(player_score, bank)
+  end
 end
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
+  "Your score is #{score}, bank is #{score}!"
 end
 
 def asking_for_card?(score)
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+puts  "Card ? (type 'Y' or 'yes' for a new card)"
+player_answer = gets.chomp
+
+if player_answer == "Y" || player_answer == "yes"
+  return true
+else
+  return false
+end
+
 end
 
 def build_message_for(outcome)
