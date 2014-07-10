e3350baadf3e165af9d9f7069c5bda070e706839
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-10 12:10:11 +0200

    completed ex enumerable

--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -26,9 +26,8 @@ end
 def short_words(array, max_length)
   # TODO: Take and array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
-array.reject do |word|
+  array.reject do |word|
       word.length > max_length
-
   end
 end
 
@@ -38,24 +37,45 @@ end
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find do |number|
+    number < limit
+  end
 end
+################################################################################
 
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
+  array.map do |string|
+    "#{string}!"
+  end
 end
+
+################################################################################
 
 def product(array)
   # TODO: Calculate the product of an array of numbers.
   #       You should use of Enumerable#reduce
+  array.reduce do |product, value|
+    product * value
+  end
+
 end
+
+################################################################################
 
 def sorted_pairs(array)
   # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
   #       You should make use of Enumerable#each_slice
+  result = []
+  array.each_slice(2) do |couple|
+    result << couple.sort
+  end
+result
 end
 
+p sorted_pairs(["c", "z", "d"])
 
-puts sum_odd_indexed([1, 2, 3, 4, 5, 6])
 
-puts even_numbers(1, 2, 3, 4, 5, 6)
+
+
