bc42305bff74362e36f5c59de5425fde33630d34
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-10 16:32:59 +0200

    exo3beautifymycode

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,14 +1,7 @@
 def shuffle_word(a_word)
   #TODO: refactor this method
 
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
-
-  shuffle_array
-
-  a_word.upcase.chars.shuffle
+  [a_word.upcase].shuffle
 
 end
 
@@ -16,5 +9,11 @@ end
 def quote_prime_numbers(n)
   #TODO: refactor this method
 
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+ variable_prime = (1..n).find_all do |i|
+  (2..i-1).select {|k| i % k == 0}.count == 0
+  end
+
+  variable_prime.map do |prime_num|
+    "#{prime_num} is prime"
+  end
 end
