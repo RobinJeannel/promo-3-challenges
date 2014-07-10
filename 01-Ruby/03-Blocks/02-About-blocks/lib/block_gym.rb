486e184d1fe7c2daee2463651804cdee8a525c5f
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-10 15:21:53 +0200

    exercice about blocks yield

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,12 +1,40 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+
+  if attr == nil
+    full_html = "<#{tag_name}>#{yield}</#{tag_name}>"
+  else
+    full_html = "<#{tag_name} #{attr[0]}='#{attr[1]}'>#{yield}</#{tag_name}>"
+  end
+
+  full_html
+
 end
 
 def timer_for
   #TODO:  Return time taken to execute the given block
+  start_time = time.now
+  yield
+  end_time = time.now - start_time
 end
 
 def transform(element)
   #TODO:  Simply execute the given block on element
+  yield(element)
 end
+
+
+
+html = tag("h1") { "HELLO" }
+
+puts html
+
+html = tag("h1") do
+  tag("a", ["href", "http://lewagon.org"]) { "le wagon" }
+end
+
+
+
+puts html
+
