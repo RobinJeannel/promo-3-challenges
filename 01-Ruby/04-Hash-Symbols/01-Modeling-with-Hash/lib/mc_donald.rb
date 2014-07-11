4d239391da68735ac76b06d1482d77f6d483f68f
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-11 14:18:34 +0200

    exoMacDopartie1done

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -3,18 +3,17 @@ def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
 
 products = {
-"Cheese Burger" => 290,
-"Big Mac" => 300,
-"Mc Bacon" => 400,
-"Royal Cheese" => 130,
-"French fries" : 130,
-"Potatoes" => 130,
-"Coca" => 160,
-"Sprite" => 170
+  "Cheese Burger" => 290,
+  "Big Mac" => 300,
+  "Mc Bacon" => 400,
+  "Royal Cheese" => 130,
+  "French fries" => 130,
+  "Potatoes" => 130,
+  "Coca" => 160,
+  "Sprite" => 170
 }
 
-
- total = products[burger] + products[side] + products[beverage]
+total = products[burger] + products[side] + products[beverage]
 return total
 end
 
@@ -23,17 +22,17 @@ end
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
 products = {
-"Cheese Burger" => 290,
-"Big Mac" => 300,
-"Mc Bacon" => 400,
-"Royal Cheese" => 130,
-"French fries" : 130,
-"Potatoes" => 130,
-"Coca" => 160,
-"Sprite" => 170
-"Happy Meal" => poor_calories_counter("Cheese Burger", "French fries", "Coca"),
-"Best Of Big Mac" => poor_calories_counter("Big Mac", "French fries", "Coca"),
-"Best Of Royal Cheese" => poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
+  "Cheese Burger" => 290,
+  "Big Mac" => 300,
+  "Mc Bacon" => 400,
+  "Royal Cheese" => 130,
+  "French fries" : 130,
+  "Potatoes" => 130,
+  "Coca" => 160,
+  "Sprite" => 170
+  "Happy Meal" => poor_calories_counter("Cheese Burger", "French fries", "Coca"),
+  "Best Of Big Mac" => poor_calories_counter("Big Mac", "French fries", "Coca"),
+  "Best Of Royal Cheese" => poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
 }
 
 orders.each do
@@ -42,6 +41,5 @@ orders.each do
 
 end
 
-puts poor_calories_counter("Cheese Burger", "Potatoes", "Coca")
-calories_counter("French fries", "Happy Meal", "Sprite")
+
 
