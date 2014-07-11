7abb8e08f1c34a29c780814d92bc5ddf6d5a36aa
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-11 14:28:28 +0200

    exomacdo

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -21,21 +21,23 @@ end
 ##############################################################################
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
-products = {
-  "Cheese Burger" => 290,
-  "Big Mac" => 300,
-  "Mc Bacon" => 400,
-  "Royal Cheese" => 130,
-  "French fries" : 130,
-  "Potatoes" => 130,
-  "Coca" => 160,
-  "Sprite" => 170
-  "Happy Meal" => poor_calories_counter("Cheese Burger", "French fries", "Coca"),
-  "Best Of Big Mac" => poor_calories_counter("Big Mac", "French fries", "Coca"),
-  "Best Of Royal Cheese" => poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
-}
+  products = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170,
+    "Happy Meal" => poor_calories_counter("Cheese Burger", "French fries", "Coca"),
+    "Best Of Big Mac" => poor_calories_counter("Big Mac", "French fries", "Coca"),
+    "Best Of Royal Cheese" => poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
+  }
 
-orders.each do
+  orders.each do
+
+  end
 
 end
 
