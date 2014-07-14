8a26ac545bc9915d300f30619ac29493f64d64fe
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-14 21:11:46 +0200

    exopmu

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,5 +1,19 @@
 def pmu_format!(race_array)
   #TODO: modify the given array so that it is PMU-consistent
+  def pmu_format!(race_array)
+
+  rank = race_array.size
+
+  for horse in 0...rank
+  race_array[horse] = "#{horse + 1}-#{race_array[horse]}!"
+  end
+
+race_array.reverse!
+
+end
+
+
+pmu_format! ["Abricot du Laudot", "Black Caviar", "Brigadier Gerard"]
 end
 
 
