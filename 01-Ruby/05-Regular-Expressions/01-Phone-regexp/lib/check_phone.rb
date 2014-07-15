532cf1eddb5efb04f68ddb6445df8cbce672dbb9
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-15 15:15:32 +0200

    exercicephone

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,13 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number =~ /^(0|\+33|0033)\D?[1-9](\D?\d\d){4}$/
+    return true
+
+  else
+    return false
+  end
 end
+
+end
+
+
