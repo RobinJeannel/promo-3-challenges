3d088716e0702f3886d8533b421b9247880eea03
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-15 09:10:00 +0200

    exosymbols

--- 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
@@ -9,10 +9,12 @@
 
 def are_identical_symbols_same_objects?
   # TODO: true or false?
+  true
 end
 
 def are_identical_strings_same_objects?
   # TODO: true or false?
+  false
 end
 
 
@@ -22,16 +24,24 @@ end
 
 def convert_string_to_symbol(a_string)
   # TODO: return the symbol version of "a_string"
+  a_string.to_sym
 end
 
 def convert_symbol_to_string(a_symbol)
   # TODO: return the string version of "a_symbol"
+  a_symbol.to_s
 end
 
 def me
   # TODO: return a Hash representing yourself, with keys such as age and name
+  me = {
+    name: "Robin",
+    age: 25
+  }
 end
 
 def fruits
-  # TODO: return an array of fruits
+  # TODO: return an array of fruits  fruits = ["apple", "strawberry", "mango", "banana"]
+  fruits = ["apple", "strawberry", "mango", "banana"]
+
 end
