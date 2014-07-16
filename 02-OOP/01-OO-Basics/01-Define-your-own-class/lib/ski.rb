fd010af032cb8d8d0b70faac3720cc2cfdcf45b8
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-16 12:06:11 +0200

    exoski

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/ski.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/ski.rb
@@ -1,25 +1,28 @@
 class Ski
-  attr_reader :speed, :resistance, :size
+  attr_reader :speed, :resistance, :size, :ski_waxed, :ski_equiped
 
-  def intitialize (speed, resistance, size)
+  def initialize(speed, resistance, size)
     @ski_equiped = false
     @ski_waxed= false
     @speed = speed
     @resistance = resistance
     @size = size
   end
+
+  def wax
+    @ski_waxed = true
+  end
+
+  def equiped
+    @ski_equiped = true
+  end
+
 end
 
 salomon = Ski.new(50, "A", 180)
 head = Ski.new(40, "B", 175)
 
-puts salomon.speed
-puts head.resistance
+puts salomon.ski_waxed
+salomon.wax
+puts salomon.ski_waxed
 
-def wax
-  @ski_waxed = true
-end
-
-def equiped
-  @ski_equiped = true
-end
