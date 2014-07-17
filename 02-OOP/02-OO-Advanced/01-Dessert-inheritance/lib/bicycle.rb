7471f31b2bd9490f8b6940d39b8bbf803ac86160
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-17 11:36:29 +0200

    exbicyle

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/bicycle.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/bicycle.rb
@@ -1,34 +1,34 @@
-class Bicycle  
-  attr_reader :gears, :wheels, :seats  
-  def initialize(gears = 1)  
-    @wheels = 2  
-    @seats = 1  
-    @gears = gears  
-  end  
-end  
-  
-class Tandem < Bicycle  
-  def initialize(gears)  
+class Bicycle
+  attr_reader :gears, :wheels, :seats
+  def initialize(gears = 1)
+    @wheels = 2
+    @seats = 1
+    @gears = gears
+  end
+end
+
+class Tandem < Bicycle
+  def initialize(gears)
     super(gears)
-    @seats = 2  
-  end  
+    @seats = 2
+  end
 end
 
 def quizz
   answers = []
-  
+
   b = Bicycle.new
-  t = Tandem.new(4)  
-  
+  t = Tandem.new(4)
+
   #TODO: replace "?" by the correct fixnum !
-  
-  answers << (b.gears == "?")
-  answers << (b.wheels == "?")
-  answers << (b.seats == "?")
-  
-  answers << (t.gears == "?")
-  answers << (t.wheels == "?")
-  answers << (t.seats == "?")
-  
+
+  answers << (b.gears == 1)
+  answers << (b.wheels == 2)
+  answers << (b.seats == 1)
+
+  answers << (t.gears == 4)
+  answers << (t.wheels == 2)
+  answers << (t.seats == 2)
+
   answers.inject(true) {|result, answer| result and answer}
 end
