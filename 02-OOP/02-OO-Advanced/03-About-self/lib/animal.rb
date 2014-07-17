b0a2b2226394eaa3ef49a31cf240b1eb8e6e046d
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-17 15:18:47 +0200

    exsolved

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -3,25 +3,29 @@ class Animal
   def name(name)
     @name = name
     #TODO: find good instruction
+    self
   end
-  
+
   def specie(specie)
     @specie = specie
     #TODO: find good instruction
+    self
   end
-  
+
   def color(color)
     @color = color
     #TODO: find good instruction
+    self
   end
-  
+
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
     #TODO: find good instruction
+    self
   end
-  
+
   def to_s
     "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
-  
+
 end
