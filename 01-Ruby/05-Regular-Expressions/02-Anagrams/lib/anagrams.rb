60e06dd6f1be106a05219cd23177e0e02feb96b6
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-18 16:09:12 +0200

    01-Ruby exercices

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,29 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+  if a_string.scan(/\w/).sort == another_string.scan(/\w/).sort
+    true
+  else
+    false
+  end
 end
+
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+  if string_to_h(a_string) == string_to_h(another_string)
+    true
+  else
+    false
+  end
 end
-  
+
+def string_to_h(a_string)
+  hash = {}
+  string_tab = a_string.downcase.scan(/\w/)
+
+  string_tab.each  do |x|
+  hash[x.to_sym] = string_tab.count(x)
+  end
+return hash
+end
+end
--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,6 +1,28 @@
-# Encoding: utf-8
+# Encoding: utf-8 = "ISO8859-1"
 require 'csv'
 
-def most_successfull(number, max_year, file_name)
+def most_successfull(number, max_year, filename)
   #TODO: return the number most successfull movies max_year
+  all_movies = load_movies(filename)
+
+  #filtrer les films par year et earnings
+  filtered_movies_1 = all_movies.select{|movie| [:year] < max_year}
+  filtered_movies_2 = filtered_movies_1.sort_by{|movie| -movie[:earnings] }
+  filtered_movies2.take(number)
 end
+
+
+def load_movies(filename)
+  movies = []
+  CSV.foreach(filename, encoding: "ISO8859-1") do |row|
+    title = row[0]
+    date = row[1]
+    earnings = row[2]
+
+    movies << { title: title, date: date.to_i, earnings: earnings.to_i }
+  end
+  movies
+end
+
+most_successfull(5, 1990, "lib/movies.csv")
+
