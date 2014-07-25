89834c0dd392c663c5d6d832e32760db1a6cdec5
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-25 14:22:49 +0200

    exointerface

--- 03-AR-Database/02-SQL-CRUD/lib/interface.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/interface.rb
@@ -5,23 +5,26 @@ def ask_and_get(param)
   gets.chomp
 end
 
-db_path = "db/news.sqlite"
+db_path = "lib/db/news.sqlite"
 db = SQLite3::Database.new(db_path)
 
-create_scheme(db) 
+create_scheme(db)
 
 while true
 
   puts "Hey you, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
-  puts "2. Read your posts"
-  puts "3. Delete all posts"
-  puts "4. Exit"
-  
+  puts "2. Read a post"
+  puts "3. Read your posts"
+  puts "4. Delete all posts"
+  puts "5. Delete a post"
+  puts "6. Update a post"
+  puts "7. Exit"
+
   # TODO: add other CRUD tasks if you wish!
-	choice =  gets.chomp.to_i
-	
-	case choice
+  choice =  gets.chomp.to_i
+
+  case choice
   when 1
     name = ask_and_get("name")
     source_url = ask_and_get("source url")
@@ -29,11 +32,17 @@ while true
     post = { name: name, source_url: source_url, date: Time.now, rating: rating }
     create_post(db, post)
   when 2
-    get_posts(db)
+    get_post(db, id)
   when 3
+    get_posts(db)
+  when 4
     delete_posts(db)
-  when 4 
+  when 5
+    delete_post(db, id)
+  when 6
+    update_post(db, id, name)
+  when 7
     break
-	end 
-	
+  end
+
 end
