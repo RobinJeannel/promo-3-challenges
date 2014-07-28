75b59f5c04103a71d952acff1949cde193f83ebe
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-28 11:21:54 +0200

    exmaj

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/lib/db/migrate/20131226095300_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/lib/db/migrate/20131226095300_create_posts.rb
@@ -1,5 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def up
     #TODO: your code here to create the Posts table
+    create_table :posts do |t|
+      t.string :name
+      t.string :source_url
+      t.datetime :created_at
+    end
   end
 end
+
