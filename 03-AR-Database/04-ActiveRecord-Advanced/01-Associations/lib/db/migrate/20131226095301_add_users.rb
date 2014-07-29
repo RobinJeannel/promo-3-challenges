7b88a9a572ff0db46d802ac5b22acd59c3dcc105
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-07-29 15:04:16 +0200

    ex1

---
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/db/migrate/20131226095301_add_users.rb
@@ -0,0 +1,8 @@
+class AddUsers < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string    :name
+      t.string    :email
+    end
+  end
+end
---
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/db/migrate/20131226095302_add_id_user_to_post.rb
@@ -0,0 +1,5 @@
+class AddIdUserToPost < ActiveRecord::Migration
+  def change
+    add_column :posts, :user_id, :integer
+  end
+end
